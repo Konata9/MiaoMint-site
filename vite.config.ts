0053031c2ad3fd463b9b5db54d001e339cb541ed
konata9 <maochichen@163.com>
2025-12-28 23:07:52 +0800

    patch: Fix cloudflare site deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/MiaoMint-site/',
+  base: '/',
   plugins: [
     vue(),
     tailwindcss()
